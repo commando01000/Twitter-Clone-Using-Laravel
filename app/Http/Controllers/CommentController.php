d1c8bdf74e7bdf54a07bae2ade295fbf12e5b7b8
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-06-13 12:11:16 +0300

    .

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,19 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
     //
-    public function insert(Request $request)
+    public function insert(Idea $idea)
     {
         $comment = new Comment();
         request()->validate([
             'idea' => 'required | min:5 | max:300',
         ]);
-        $comment->idea = request('idea');
-        $comment->idea_id = request('idea_id');
-        return redirect()->route('ideas.show', $comment->idea_id)->with('success', 'Comment added successfully');
+        $comment->comment = $idea->idea;
+        $comment->idea_id = $idea->id;
+        $comment->save();
+        return redirect()->route('idea.showIdea', $idea->id)->with('success', 'Comment added successfully');
     }
 }
