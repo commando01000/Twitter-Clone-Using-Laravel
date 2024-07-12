e6f20cb73dbd15d85103fb9f80f448df00606cdb
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-07-12 17:26:12 +0300

    .

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Idea;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Session;
@@ -32,8 +33,18 @@ class DashboardController extends Controller
                 ['idea', 'like', '%' . $search . '%'],
             ]);
         }
+
+        $topIdeas = $ideas->get()->sortByDesc('likes')->take(5);
+
+        $topUsers = $topIdeas->unique('user_id')->take(5)->values();
+
+        $users = User::all();
+
+        $topUsersNames = $users->whereIn('id', $topUsers->pluck('user_id'));
+
         return view('dashboard', [
             'ideas' => $ideas->paginate(5),
+            'topUsers' => $topUsersNames
         ]);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Idea;
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 
@@ -22,5 +26,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+
+
+        $topUsers = Cache::remember('topUsers', now()->addMinutes(), function () {
+            $topIdeas = Idea::with('user:id,name,image', 'comments.user:id,name,image')->latest()->get()->sortByDesc('likes')->take(5);
+            return $topIdeas->unique('user_id')->take(5)->values();
+        });
+        $users = User::all();
+        View::share('topUsers', $users->whereIn('id', $topUsers->pluck('user_id')));
     }
 }
